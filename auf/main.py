999315aa37ee35d60455cee3722b041c49233d68
Vinoggradic1 <72489102+Vinoggradic1@users.noreply.github.com>
2020-11-16 17:23:39 +1000

    Update main.py

--- auf/main.py
+++ auf/main.py
@@ -7,14 +7,14 @@ import time
 os.environ['SDL_VIDEO_WINDOW_POS'] = '0,0'
 
 pygame.init()
-FPS = 120
+FPS = 60
 clock = pygame.time.Clock()
 
 Info = pygame.display.Info()
 W, H = Info.current_w, Info.current_h
 
 MAX_SNOW = 150
-SNOW_SIZE = 64
+snow_size = 64
 BG_COLOR = (25, 25, 25)
 
 
@@ -24,14 +24,41 @@ class Snow(pygame.sprite.Sprite):
 
         self.speed = random.randint(1, 3)
         self.img_num = random.randint(1, 2)
-        self.SNOW_SIZE = random.randint(32, 64)
+        self.snow_size = random.randint(32, 64)
         self.image_filename = f'snowflake{self.img_num}.png'
         self.image_orig = pygame.image.load(self.image_filename)
-        self.image_orig = pygame.transform.scale(self.image_orig, (self.SNOW_SIZE, self.SNOW_SIZE))
+        self.image_orig = pygame.transform.scale(self.image_orig, (self.snow_size, self.snow_size))
         self.image = self.image_orig.copy()
-        self.rect = self.image.get.rect(center=(x, y))
+        self.rect = self.image.get_rect(center=(x, y))
         self.rot = 0
         self.angle = random.randint(-1, 1)
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top > H:
+            self.rect.bottom = 0
+
+        n = random.randint(1, 20)
+        if n == 1:
+            self.rect.x += 1
+            if self.rect.left > W:
+                self.rect.right = 0
+        elif n == 2:
+            self.rect.x -= 1
+            if self.rect.left < 0:
+                self.rect.left = W
+        else:
+            self.rect.x += 0
+        
+        self.rot = (self.rot + self.angle) % 360
+        self.image = pygame.transform.rotate(self.image_orig, self.rot)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+
+def init_snow(max_snow):
+    for _ in range(MAX_SNOW):
+        snow = Snow(random.randint(0, W), random.randint(0, H))
+        all_snow.add(snow)
 
 
 def check_for_exit():
@@ -40,11 +67,19 @@ def check_for_exit():
             sys.exit(0)
 
 
+
+
 pygame.display.set_icon(pygame.image.load('snow.ico'))
 pygame.display.set_caption('SNOW')
 screen = pygame.display.set_mode((W, H))
 
+all_snow = pygame.sprite.Group()
+init_snow(MAX_SNOW)
+
 while True:
     check_for_exit()
+    all_snow.update()
+    screen.fill(BG_COLOR)
+    all_snow.draw(screen)
     pygame.display.update()
     clock.tick(FPS)
